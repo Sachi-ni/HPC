365559513fb53ba0ee8224e6b8730a05d6953792
Weerasingha R.P.G.S.N <nisansalaweerasingha3848@gmail.com>
2026-08-14 16:19:26 +0530

    Create PI-Serial.c

---
+++ PI-Serial.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <omp.h>
+
+#define NT 8
+
+long N = 100000000;
+double dx;
+
+int main()
+{
+	int i;
+	double pi = 0.0;
+	//double sum = 0.0;
+        double start_time, run_time;
+	dx = 1.0 / (double)N;
+
+	start_time = omp_get_wtime();
+        
+        #pragma omp parallel num_threads(NT)
+        {
+	 
+            int myid = omp_get_thread_num();	
+            double sum = 0.0;
+            double x;
+	    for (i = myid; i < N; i+=NT) {
+		      x = (i + 0.5) * dx;
+		      sum += 4.0 / (1.0 + x * x);  //this one is safe, bcz we used private variable :they have their own copy
+	      }
+	      #pragma omp critical
+	          pi += sum*dx;
+        }
+       
+        
+	run_time = omp_get_wtime() - start_time;
+	printf("\n pi with %ld steps is %lf in %lf seconds\n ", N, pi, run_time);
+}
+
+//inside the for loop do not share critical values
+//The important point is that the for loop does not need a critical section because each thread is working on its own private sum.
