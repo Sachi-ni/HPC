77eee55323a44c5f1821e374f00c514326b132c2
Weerasingha R.P.G.S.N <nisansalaweerasingha3848@gmail.com>
2026-08-14 15:43:34 +0530

    Create multiple threads safely update a shared variable.cpp

---
+++ multiple threads safely update a shared variable.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <omp.h>
+
+#define NT 8
+
+int main() {
+    long final_sum = 0;     
+    //long psum[NT] = {0};
+    
+    double start_time = omp_get_wtime(); 
+
+    #pragma omp parallel num_threads(NT)
+    {
+        int myid = omp_get_thread_num();
+        long psum = 0;
+        
+        for (int i = 0; i < 1000000; i++) {
+            psum++; 
+        }
+        #pragma omp critical
+          final_sum = final_sum+psum;
+    }
+    
+    
+    
+    double end_time = omp_get_wtime(); 
+
+    printf("Final value of shared_variable is %ld.\n", final_sum);
+    printf("Runtime: %f seconds\n", end_time - start_time);
+
+    return 0;
+}
