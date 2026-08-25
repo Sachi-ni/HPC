00f19d7c62cc5f42c8554a17bcef0121dd1e8572
Weerasingha R.P.G.S.N <nisansalaweerasingha3848@gmail.com>
2026-08-25 16:07:00 +0530

    Create Array Sum Computation.c

---
+++ Array Sum Computation.c
@@ -0,0 +1,22 @@
+#include <omp.h>
+#include <stdio.h>
+
+#define N 10
+
+
+int main() {
+    int A[N] = {1,2,3,4,5,6,7,8,9,10};
+    int s=0;
+    
+    //#pragma omp parallel
+   /* {
+          #pragma omp for reduction(+:s)
+          for(int i=0; i<N; i++)
+                s+=A[i];
+              
+         
+    }*/
+    printf("Sum =%d\n",s);
+
+    return 0;
+}
